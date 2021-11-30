562ecd9f7a864def889cb83c5e64661e83a64414
Alina <42976128+lesslyrics@users.noreply.github.com>
2021-11-30 22:58:05 +0300

    Migrated to omp tasks

--- main.cpp
+++ main.cpp
@@ -15,68 +15,56 @@ const int MAX_VAL = 5;
 // LU decomposition
 // a = l * u
 // array size = size x size
-void lu_decomposition(float** a, float** l, float** u, int size)
-{
+void lu_decomposition(float **a, float **l, float **u, int size) {
     // initialize a simple lock for parallel region
     omp_lock_t lock;
 
     omp_init_lock(&lock);
 
 // for loops of lu decomposition are executed in parallel
-#pragma omp parallel shared(a,l,u)
-    {
-        for (int i = 0; i < size; i++)
-        {
-// rows are processed in separate threads for l-matrix
-#pragma omp for schedule(static)
-            for (int j = 0; j < size; j++)
-            {
-                // right-upper part
-                if (j < i)
-                {
-                    l[j][i] = 0;
-                    continue;
-                }
-                l[j][i] = a[j][i];
-                for (int k = 0; k < i; k++)
-                {
-                    l[j][i] = l[j][i] - l[j][k] * u[k][i];
-                }
+#pragma omp taskloop shared(a, l, u)
+    for (int i = 0; i < size; i++) {
+#pragma omp task shared(a, l, u)
+        for (int j = 0; j < size; j++) {
+            // right-upper part
+            if (j < i) {
+                l[j][i] = 0;
+                continue;
             }
-
-// rows are processed in separate threads for u-matrix
-#pragma omp for schedule(static)
-            for (int j = 0; j < size; j++)
-            {
-                // left-bottom part
-                if (j < i)
-                {
-                    u[i][j] = 0;
-                    continue;
-                }
-                // diagonal is set to 1
-                if (j == i)
-                {
-                    u[i][j] = 1;
-                    continue;
-                }
-                u[i][j] = a[i][j] / l[i][i];
-                for (int k = 0; k < i; k++)
-                {
-                    u[i][j] = u[i][j] - ((l[i][k] * u[k][j]) / l[i][i]);
-                }
-
+            l[j][i] = a[j][i];
+            for (int k = 0; k < i; k++) {
+                l[j][i] = l[j][i] - l[j][k] * u[k][i];
             }
         }
+#pragma omp taskwait
+// rows are processed in separate threads for u-matrix
+#pragma omp task shared(a, l, u)
+        for (int j = 0; j < size; j++) {
+            // left-bottom part
+            if (j < i) {
+                u[i][j] = 0;
+                continue;
+            }
+            // diagonal is set to 1
+            if (j == i) {
+                u[i][j] = 1;
+                continue;
+            }
+            u[i][j] = a[i][j] / l[i][i];
+            for (int k = 0; k < i; k++) {
+                u[i][j] = u[i][j] - ((l[i][k] * u[k][j]) / l[i][i]);
+            }
+
+        }
     }
+#pragma omp taskwait
+
 }
 
-void init_matrices(float** a, float** l, float** u, int size)
-{
+void init_matrices(float **a, float **l, float **u, int size) {
 // initialize matrices in separate threads
 #pragma omp for schedule(static)
-    for (int i = 0; i < size; ++i)
-    {
+    for (int i = 0; i < size; ++i) {
         a[i] = new float[size];
         l[i] = new float[size];
         u[i] = new float[size];
@@ -84,28 +72,23 @@ void init_matrices(float** a, float** l, float** u, int size)
 }
 
 // fill matrix with random values
-void random_fill(float** matrix, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            matrix[i][j] = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/MAX_VAL));
+void random_fill(float **matrix, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            matrix[i][j] = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / MAX_VAL));
         }
     }
     make_sure_matrix_is_invertible(matrix, size);
 
 }
 
-void make_sure_matrix_is_invertible(float** matrix, int size) {
+void make_sure_matrix_is_invertible(float **matrix, int size) {
     // to make sure that the matrix is invertible, it should be diagonal dominant
     float leftovers_sum = 0;
     int position_on_diag = 0; // column on the diagonal
 
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             leftovers_sum += abs(matrix[i][j]);
         }
         // remove the diagonal value
@@ -117,26 +100,20 @@ void make_sure_matrix_is_invertible(float** matrix, int size) {
     }
 }
 
-void print_matrix(float** matrix, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            cout << left << setw(9) << setprecision(3) << matrix[i][j] << left <<  setw(9);
+void print_matrix(float **matrix, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            cout << left << setw(9) << setprecision(3) << matrix[i][j] << left << setw(9);
         }
         cout << endl;
     }
 }
 
-float find_det(float** matrix, int size)
-{
-    float det = 1;
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            if (i == j){
+long double find_det(float **matrix, int size) {
+    long double det = 1;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i == j) {
                 det *= matrix[i][j];
             }
         }
@@ -146,45 +123,57 @@ float find_det(float** matrix, int size)
 }
 
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
     double runtime;
     // number of threads to use
-    omp_set_num_threads(30);
+    int number_of_threads = 16;
+    omp_set_num_threads(number_of_threads);
 
     // size of matrix
     int size = atoi(argv[1]);
 
-    cout << "Matrix size: " <<  size << endl;
+    cout << "Matrix size: " << size << endl;
 
     // seed for random
     srand(1);
 
-    auto** A = new float* [size];
-    auto** L = new float* [size];
-    auto** U = new float* [size];
+    auto **A = new float *[size];
+    auto **L = new float *[size];
+    auto **U = new float *[size];
 
     init_matrices(A, L, U, size);
 
     // fill A with random values
     random_fill(A, size);
-    cout << "Matrix A: " << endl;
-    print_matrix(A, size);
+
+//    cout << "Matrix A: " << endl;
+//    print_matrix(A, size);
 
     runtime = omp_get_wtime();
-    lu_decomposition(A, L, U, size);
 
-    // results
-    cout << "Matrix L: " << endl;
-    print_matrix(L, size);
-    cout << "Matrix U:" << endl;
-    print_matrix(U, size);
+
+#pragma omp parallel
+    {
+#pragma omp single
+        {
+#pragma omp task
+            { lu_decomposition(A, L, U, size); }
+        }
+    }
+
+//    results for testing purposes
+//    cout << "Matrix L: " << endl;
+//    print_matrix(L, size);
+//    cout << "Matrix U:" << endl;
+//    print_matrix(U, size);
 
     // det(A) = det(L) * det(U), det(U) = 1 => det(A) = det(L)
-    float det = find_det(L, size);
+    long double det = find_det(L, size);
     cout << "Det of LU = " << det << endl;
 
     // time
-    cout << "Runtime result: " << omp_get_wtime() - runtime << endl;
+    cout << "Runtime result for the size " << size << " and number of threads = " << number_of_threads << ": " <<
+         omp_get_wtime() - runtime << endl;
+
     return 0;
 }
